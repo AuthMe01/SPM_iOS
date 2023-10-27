2376f09ccbb1a99b982a8ef658a8cf2960e5dedb
Evan Wu <evan.wu@authme.com>
2023-10-27 14:02:35 +0800

    [Update] 2.4.6

--- Package.swift
+++ Package.swift
@@ -27,17 +27,17 @@ let package = Package(
         .binaryTarget(
             name: "AuthMe",
             url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMe.xcframework.zip",
-            checksum: "cf690d7e82064c8a759611ee27f2a66ea3bdd0ee3da34a4bc26c5f0b9cc6ebc2"
+            checksum: "292d2705db02bde0f5438797c8cb7b097e6785785d9ed555186b2f4338e03f5f"
         ),
         .binaryTarget(
             name: "AuthMeUI",
             url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/AuthMeUI.xcframework.zip",
-            checksum: "4322ad55c1cd1fc55001c6d0b4f968ac2787ef519735b8929dc49d802e6e98e9"
+            checksum: "084ad57396077b9ef114b52c7a4d5e387ee6a5aa083c436f23fc6bade6d84df4"
         ),
         .binaryTarget(
             name: "Algo",
             url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/2.4.6/Algo.xcframework.zip",
-            checksum: "13bd74570f18713f2434e14414a260c2a55d559c48962f206abc395d68bf4955"
+            checksum: "4caa724b2d15af56f6d65671df47e28adfe982206205a62889d5ffd68423c2eb"
         ),
         .binaryTarget(
             name: "OpenSSL",
