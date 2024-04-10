02f57a6800feb1cefce4dd61f34593058afb3bc1
AuthMe-Team <dev@authme.com>
2024-04-10 18:43:14 +0800

    add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AuthMeSPM",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "AuthMeSPM",
