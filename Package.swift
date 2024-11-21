e797dc8c57ea7bc6395434ff352fbb5267629fa2
AuthMe-Team <dev@authme.com>
2024-11-21 08:07:28 +0000

    Release 0.2.2

--- Package.swift
+++ Package.swift
@@ -25,13 +25,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AuthMe",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/core/0.2.1/AuthMe.xcframework.zip",
-            checksum: "7b2c1c9af2f7efdb5c0d78831bf4299505012bf0d25912667b75af6efd58bdc9"
+            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/core/0.2.2/AuthMe.xcframework.zip",
+            checksum: "dc9f76041ce686c46dd748b3d07765bad0a144c99eeafd3ffdecb76ee404e754"
         ),
         .binaryTarget(
             name: "AuthMeUI",
-            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/core/0.2.1/AuthMeUI.xcframework.zip",
-            checksum: "e0c3fb53da3a58d8e22fc71e0e302b8fc405417d127c8c8cde2a7415ccf786a6"
+            url: "https://storage.googleapis.com/authme-mobile.appspot.com/iOS/core/0.2.2/AuthMeUI.xcframework.zip",
+            checksum: "fe44c378909b98401579d7a2a3b8dfcb2b02e7ab39120e60ecf755d42c512281"
         ),
         .binaryTarget(
             name: "Algo",
